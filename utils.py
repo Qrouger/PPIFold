3764b58ae40e268b4de0b7c2435530ba6b09321c
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2024-08-02 11:40:15 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -175,7 +175,7 @@ def add_iQ_score_and_make_cyt (dir_alpha) :
         ----------
 
         """
-        cmd4 = f"singularity exec --no-home --bind result_all_vs_all:/mnt {dir_alpha}/alpha_analysis_jax_0.4.sif run_get_good_pae.sh --output_dir=/mnt --cutoff=10"
+        cmd4 = f"singularity exec --no-home --bind result_all_vs_all:/mnt {dir_alpha}/alpha-analysis_jax_0.4.sif run_get_good_pae.sh --output_dir=/mnt --cutoff=10"
         os.system(cmd4)
         with open("result_all_vs_all/predictions_with_good_interpae.csv", "r") as file1 :
             reader = csv.DictReader(file1)
@@ -351,7 +351,7 @@ def add_hiQ_score (dir_alpha) :
         ----------
 
         """
-        cmd4 = f"singularity exec --no-home --bind result_homo_oligo:/mnt {dir_alpha}/alpha_analysis_jax_0.4.sif run_get_good_pae.sh --output_dir=/mnt --cutoff=10"
+        cmd4 = f"singularity exec --no-home --bind result_homo_oligo:/mnt {dir_alpha}/alpha-analysis_jax_0.4.sif run_get_good_pae.sh --output_dir=/mnt --cutoff=10"
         os.system(cmd4)
         with open("result_homo_oligo/predictions_with_good_interpae.csv", "r") as file1 :
             reader = csv.DictReader(file1)
