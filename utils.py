9a332c435a2b2e1bc4eb8af49097f934c44fd800
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2024-10-21 19:15:32 +0200

    Plot normalized heatmap

--- utils.py
+++ utils.py
@@ -447,6 +447,45 @@ def generate_interaction_network (file) :
     plt.savefig("network.png")
     plt.close()
 
+
+#def generate_interaction_network2 (file) :
+ #   interactions = list()
+  #  with open("result_all_vs_all/predictions_with_good_interpae.csv", "r") as file1 :
+   #     reader1 = csv.DictReader(file1)
+    #    for row in reader1 :
+     #       names = row['jobs'].split('_and_')
+      #      if names not in [x[0] for x in interactions] and float(row['iQ_score']) >= 35 :
+       #         interactions.append([names, float(row['iQ_score'])])
+#    best_homo = dict()
+ #   with open("result_homo_oligo/predictions_with_good_interpae.csv", "r") as file2 :
+  #      reader2 = csv.DictReader(file2)
+   #     for row in reader2 :
+    #        if float(row["hiQ_score"]) >= 50 :
+     #           prot_name = row['jobs'].split("_homo_")[0]
+      #          if prot_name not in best_homo.keys() or float(row['hiQ_score']) >= best_homo[prot_name][0] :
+       #             number_homo = int((row['jobs'].split("homo_")[1]).split("er")[0]) #to take the number of homo-oligomerisation of the protein and this score
+        #            best_homo[prot_name] = (float(row['hiQ_score']),number_homo) #to take the number of homo-oligomerisation of the protein and this score
+#    for key in best_homo :
+ #       interactions.append([[key,key], best_homo[key][1]])
+  #  H = xgi.Hypergraph()
+   # list_inter = list()
+    #prots = set()
+#    dict_name = file.get_names()
+ #   for inter, score in interactions :
+  #      inter0 = inter[0]#+f"({dict_name[inter[0]]})" #set uniprotID with the name of protein
+   #     inter1 = inter[1]#+f"({dict_name[inter[1]]})"
+    #    prots.add(inter0)
+     #   prots.add(inter1)
+      #  list_inter.append([inter0,inter1])
+#    prots = list(prots)
+ #   H.add_nodes_from(prots)
+  #  H.add_edges_from(list_inter)
+   # pos = xgi.barycenter_spring_layout(H, seed=1)
+    #print(H.edge_id())
+#    xgi.draw(H, node_labels=True,hyperedge_labels=True, pos=pos)
+ #   plt.savefig("network.png")
+  #  plt.close()
+
 def generate_heatmap (file):
     """
     Generate heatmap of interactions scores.
@@ -461,6 +500,7 @@ def generate_heatmap (file):
     data_matrix = list()
     iQ_score_dict = file.get_iQ_score_dict()
     proteins_list = file.get_proteins()
+    save_line = dict()
     for protein1 in proteins_list :
         line = [] 
         for protein2 in proteins_list :
@@ -472,9 +512,28 @@ def generate_heatmap (file):
                 elif (protein2,protein1) in iQ_score_dict.keys() :
                     line.append(float(iQ_score_dict[(protein2,protein1)]))
                 else :
+                    line.append(float(0))
                     print (protein1 + " and " + protein2 + " are not in score table")
+        save_line[protein1] = line
         data_matrix.append(line)
     complet_matrix = pd.DataFrame(data_matrix,index = proteins_list, columns = proteins_list )
     seaborn.heatmap(complet_matrix, cbar_kws = {'label' : 'iQ_score'})
     plt.savefig("heatmap.png")
+    plt.close()
+    norm_data_matrix = list()
+    for protein in proteins_list :
+        new_line = list()
+        sum_iQ = 0
+        max_iQ = 0
+        for iQ_score in save_line[protein] :
+            sum_iQ += iQ_score
+            if iQ_score > max_iQ :
+                max_iQ = iQ_score
+        ave_iQ = sum_iQ / len(save_line)
+        for iQ_score in save_line[protein] :
+            new_line.append(((((iQ_score - ave_iQ) / max_iQ)+1)*0.5)*100)
+        norm_data_matrix.append(new_line)
+    complet_matrix2 = pd.DataFrame(norm_data_matrix, index = proteins_list, columns = proteins_list )
+    seaborn.heatmap(complet_matrix2, cbar_kws = {'label' : 'iQ_score*'})
+    plt.savefig("Normalizd_heatmap.png")
     plt.close()
