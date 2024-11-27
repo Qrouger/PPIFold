24aafaed398f4c193e4ed5d7e8df4de3490d925d
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2024-11-27 14:55:38 +0100

    Delete test directory

--- test/test_data/predictions/pi_score_outputs/TEST_and_TEST_and_TEST/ranked_0/ranked_0_clean.pdb_C_B_sc_infle.sh
+++
@@ -1,7 +0,0 @@
-sc XYZIN ranked_0_clean.pdb <<eof 
-MOLECULE 1
-CHAIN C
-MOLECULE 2
-CHAIN B
-END
-eof
--- test/test_data/predictions/pi_score_outputs/TEST_and_TEST_and_TEST/ranked_0/ranked_0_clean.pdb_D_B_sc_infle.sh
+++
@@ -1,7 +0,0 @@
-sc XYZIN ranked_0_clean.pdb <<eof 
-MOLECULE 1
-CHAIN D
-MOLECULE 2
-CHAIN B
-END
-eof
