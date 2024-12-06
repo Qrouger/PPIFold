62c5ce2d9ba1056124a58e41e0172a5bf43638a4
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2024-12-06 15:33:40 +0100

    Update and rename setup.cfg to setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""Install script for setuptools."""
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='PPIFold',
+    version=0.0.1,
+    description=(
+        'Pipeline that automates AlphaPulldown'
+    ),
+    author='Quentin Rouger',
+    author_email='quentin.rouger@univ-rennes.fr',
+    license='GPL-3.0 license',
+    url='https://github.com/Qrouger/PPIFold',
+    packages=find_packages(),
+    install_requires=[
+        'alphapulldown'
+    ]
+)
