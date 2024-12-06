b906fcb89fdf936e0ef4fc9adefb88de2ac90aad
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2024-12-06 15:43:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     author_email='quentin.rouger@univ-rennes.fr',
     license='GPL-3.0 license',
     url='https://github.com/Qrouger/PPIFold',
+    include_package_data=True,
     packages=find_packages(),
     install_requires=[
         'alphapulldown'
