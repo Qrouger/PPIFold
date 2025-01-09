ae9af148bc15cbcccd7669249177fcd8be587c2e
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2025-01-09 13:37:20 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""Install script for setuptools."""
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='PPIFold',
+    version='0.0.9',
+    description=(
+        'Automatic pipeline using AlphaPulldown to predict PPI and homo-oligomer'
+    ),
+    author='Quentin Rouger',
+    author_email='quentin.rouger@univ-rennes.fr',
+    license='GPL-3.0 license',
+    url='https://github.com/Qrouger/PPIFold',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        'alphapulldown==2.0.0',
+        'seaborn',
+        'urllib3',
+        'matplotlib',
+        'scipy'
+    ],
+    entry_points={'console_scripts': ['PPIFold=PPIFold.PPIFold:main',],}
+)
