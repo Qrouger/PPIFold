85dd7b89b40b22828ad295da9b1d36ecc414d3af
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2025-01-10 09:13:55 +0100

    Update Utils.py

--- PPIFold/Utils.py
+++ PPIFold/Utils.py
@@ -385,14 +385,15 @@ def add_hiQ_score (dir_alpha) :
     """
     cmd4 = f"singularity exec --no-home --bind result_homo_oligo:/mnt {dir_alpha}/alpha-analysis_jax_0.4.sif run_get_good_pae.sh --output_dir=/mnt --cutoff=10"
     os.system(cmd4)
-    with open("predictions_with_good_interpae.csv", "r") as file1 :
+    with open("./result_homo_oligo/predictions_with_good_interpae.csv", "r") as file1 :
         reader = csv.DictReader(file1)
         all_lines = "jobs,pi_score,iptm_ptm,hiQ_score\n"
         all_homo = dict()
         save_pi_score = dict()
         for row in reader :
             job = row['jobs']
-            if 'homo' in job and row['pi_score'] != 'No interface detected' :
+            #if 'homo' in job and row['pi_score'] != 'No interface detected' : #need AFPD release with homo-oligo ####_homo_2er
+            if row['pi_score'] != 'No interface detected' :
                 if job not in all_homo.keys() :
                     all_homo[job] = (row['pi_score'],1,row)
                     save_pi_score[job] = [float(row['pi_score'])]
@@ -416,7 +417,7 @@ def add_hiQ_score (dir_alpha) :
             hiQ_score = (((float(all_homo[key][0])/all_homo[key][1])+2.63)/5.26)*60+float(row['iptm_ptm'])*40 #cause iptm_ptm is always same for each homo of same protein
             line = key+","+str(all_homo[key][0])+","+row['iptm_ptm']+","+str(hiQ_score)+"\n"
             all_lines += line
-    with open("new_predictions_with_good_interpae.csv", "w") as file2 :
+    with open("./result_homo_oligo/new_predictions_with_good_interpae.csv", "w") as file2 :
         file2.write(all_lines)
 
 def generate_interaction_network (file) :
