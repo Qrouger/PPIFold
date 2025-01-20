d4e2ec1a6517410f1a11fd7ff27629eb17eacb51
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2025-01-20 15:58:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='PPIFold',
-    version='0.2.1',
+    version='0.2.2',
     description=(
         'Automatic pipeline using AlphaPulldown to predict PPI and homo-oligomer'
     ),
