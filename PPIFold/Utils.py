db78f23014738594698d1a7fcb761d37813e94a7
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2025-02-12 12:58:57 +0100

    Update Utils.py

--- PPIFold/Utils.py
+++ PPIFold/Utils.py
@@ -228,6 +228,7 @@ def Make_all_vs_all (data_dir, Path_Pickle_Feature) :
     os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
     os.environ['TF_FORCE_UNIFIED_MEMORY'] = 'true'
     os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '3.2'
+    os.environ['XLA_FLAGS'] = '--xla_gpu_enable_triton_gemm=false'
     cmd1 =f"run_multimer_jobs.py --mode=custom \--num_cycle=3 \--num_predictions_per_model=1 \--compress_result_pickles=True \--output_path=./result_all_vs_all \--data_dir={data_dir} \--protein_lists=all_vs_all.txt \--monomer_objects_dir={Path_Pickle_Feature} \--remove_keys_from_pickles=False"
     os.system(cmd1)
 
@@ -465,6 +466,7 @@ def Make_homo_oligo (data_dir, Path_Pickle_Feature) :
     os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
     os.environ['TF_FORCE_UNIFIED_MEMORY'] = 'true'
     os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '3.2'
+    os.environ['XLA_FLAGS'] = '--xla_gpu_enable_triton_gemm=false'
     cmd1 =f"run_multimer_jobs.py --mode=custom \--num_cycle=3 \--num_predictions_per_model=1 \--compress_result_pickles=True \--output_path=./result_homo_oligo \--data_dir={data_dir} \--protein_lists=homo_oligo.txt \--monomer_objects_dir={Path_Pickle_Feature} \--remove_keys_from_pickles=False"
     os.system(cmd1)
 
