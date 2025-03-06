80f8bbcc61c13eb2645343618774d143a414fa54
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2025-03-06 15:23:11 +0100

    Update Utils.py

--- PPIFold/Utils.py
+++ PPIFold/Utils.py
@@ -673,7 +673,10 @@ def cluster_interface (file) :
                 already_inter.append(alphabet[0])
             for interface2 in range(interface1+1,len(interface_dict[proteins])) :
                 list_inter = list(set(interface_dict[proteins][interface1]).intersection(set(interface_dict[proteins][interface2])))
-                simi_inter = len(list_inter)/(len(set(interface_dict[proteins][interface1]).union(set(interface_dict[proteins][interface2])))-3) #indice jaccard # -3 just to remove interface 'a' and uniprotID from .union()
+                if len(set(interface_dict[proteins][interface1]).union(set(interface_dict[proteins][interface2]))) == 0 : #if no similar residue
+                   simi_inter = 0
+                else :
+                   simi_inter = len(list_inter)/(len(set(interface_dict[proteins][interface1]).union(set(interface_dict[proteins][interface2])))-3) #indice jaccard # -3 just to remove interface 'a' and uniprotID from .union()
                 if simi_inter < 0.35: #create a new interface
                     if interface_dict[proteins][interface2][0] in already_inter : #Don't create new interface if it already has one
                         pass
