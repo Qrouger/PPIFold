bba6dec7949ae16125f7e416ce3b8cfce74668cb
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2025-03-08 19:54:31 +0100

    Update PPIFold.py

--- PPIFold/PPIFold.py
+++ PPIFold/PPIFold.py
@@ -27,14 +27,14 @@ def main() :
     #PPI_object.find_proteins_sequence()
     #PPI_object.find_prot_lenght()
     if len(PPI_object.already_pickle(path_dict["Path_Pickle_Feature"])) > 0 : #if new feature pickle is need
-        PPI_object.find_proteins_sequence()
         PPI_object.create_fasta_file()
         if args.use_signalP == True :
             remove_SP(PPI_object,args.org)
         create_feature(PPI_object,path_dict["Path_AlphaFold_Data"],path_dict["Path_Pickle_Feature"],args.use_mmseq)
         Make_all_MSA_coverage(PPI_object,path_dict["Path_Pickle_Feature"])
     else :
-        recover_prot_lenght()#PPI_object.find_proteins_sequence()  #PPI_object.find_prot_lenght()
+        recover_prot_sequence()#PPI_object.find_proteins_sequence()  #PPI_object.find_prot_lenght()
+    PPI_object.find_prot_lenght()
     generate_APD_script(PPI_object, args.max_aa)
     if args.make_multimers == True :
         Make_all_vs_all(path_dict["Path_AlphaFold_Data"],path_dict["Path_Pickle_Feature"])
