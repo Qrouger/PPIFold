e70864c9571c8ddbf6360ba284338256e49776e1
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2025-07-21 13:51:13 +0200

    Update Utils.py

--- PPIFold/Utils.py
+++ PPIFold/Utils.py
@@ -480,7 +480,7 @@ def Make_homo_oligo (data_dir, Path_Pickle_Feature) :
     ----------
     """
     os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
-    os.environ['TF_FORCE_UNIFIED_MEMORY'] = 'true'
+    os.environ['TF_FORCE_UNIFIED_MEMORY'] = '1'
     os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '3.2'
     os.environ['XLA_FLAGS'] = '--xla_gpu_enable_triton_gemm=false'
     cmd1 =f"run_multimer_jobs.py --mode=custom \--num_cycle=3 \--num_predictions_per_model=1 \--compress_result_pickles=True \--output_path=./result_homo_oligo \--data_dir={data_dir} \--protein_lists=homo_oligo.txt \--monomer_objects_dir={Path_Pickle_Feature} \--remove_keys_from_pickles=False"
