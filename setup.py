f260f794d4ad5032dcab743b27ff156121454ab8
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2025-09-09 11:32:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     include_package_data=True,
     packages=find_packages(),
     install_requires=[
-        'alphapulldown',
+        'alphapulldown==2.0.4',
         'seaborn',
         'matplotlib',
         'scipy',
