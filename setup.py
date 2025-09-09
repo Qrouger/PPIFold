b7083903ddcaacb0ab396d683174c9fa2dca5332
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2025-09-09 11:33:01 +0200

    Bump 1.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='PPIFold',
-    version='1.0.1',
+    version='1.0.2',
     description=(
         'Automatic pipeline using AlphaPulldown to predict PPI and homo-oligomer'
     ),
