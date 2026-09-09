e6826a4abccc5904d2baeee9f9a88574401dddcf
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2026-09-09 10:44:02 +0200

    Bump version from 1.0.7 to 1.0.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='PPIFold',
-    version='1.0.7',
+    version='1.0.8',
     description=(
         'Automatic pipeline using AlphaPulldown to predict PPI and homo-oligomer'
     ),
